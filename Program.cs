ac91d441b1edc1963c03644534b5db2949d304f6
Ricardo Rocha <giordani.ricardo@hotmail.com>
2025-11-11 06:27:55 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
-using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// ✅ Pega a string de conexão do appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("ConexaoPadrao");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    Console.WriteLine("⚠️ ERRO: ConnectionString não encontrada no appsettings.json!");
+}
+else
+{
+    Console.WriteLine($"ConnectionString: {connectionString}");
+}
+
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseSqlServer(connectionString));
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,9 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
